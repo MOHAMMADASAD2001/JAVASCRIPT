8c84ecafcdfbfa9a46acf7e76205eb45ba5bbfaf
MOHAMMADASAD2001 <mohammad.assad0101@gmail.com>
2023-07-09 23:03:36 +0300

    task

---
+++ task-9-july/task1.js
@@ -0,0 +1,50 @@
+
+let zakat = 250 ; 
+        console.log(zakat * (2.5 / 100));
+
+        
+        let m = [1, 7, 9, 45];  
+        let w = ["str", "alex", "moh", "the", "fox", "over", "lazy", "dog "] 
+        console.log(m);
+        console.log(w);
+
+        var fruits=["Tomato","Banana","Watermelon"];
+        console.log(fruits.indexOf("Banana"));
+console.log(fruits.indexOf("Tomato"));
+console.log( fruits.indexOf("Watermelon"));
+
+let Food = ["Pizza, Mansaf"];
+let Sport = ["Football, playing football, watching football"];
+let Movie = ["Fast X, John wick"];
+console.log(Food);
+console.log(Sport);
+console.log(Movie);
+
+let number =["12, 13, 14"];
+console.log(number[0]);
+
+let no = [12, 20, 30];
+console.log(no[no.length-3]);
+
+var array = [0,5,7,9];
+array.shift();
+array.shift();
+array.shift();
+array.unshift(1,3,4,6,8);
+array.push(10);
+console.log(array)
+
+var array2 =[0,5,7,9];
+array2.shift();
+    array2.shift();
+    array2.shift();
+    array2.unshift(5);
+    array2.push(-7);
+    array2.push(3.5);
+    console.log(array2);
+
+    var arr1 = [-3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+    arr1 = arr1.sort(function (a,b) {;
+        return(a-b)});
+        console.log(arr1);
+    
